31ae69c99a80b2897bb1f98ead05d5f53afcef99
Treyce Wheeler <t.zeryon@gmail.com>
2022-08-10 18:43:39 -0400

    Add win condition check, tie check, disable ability to make move when game won

--- script.js
+++ script.js
@@ -13,18 +13,48 @@ const gameBoard = (() => {
         move[3], move[4], move[5],
         move[6], move[7], move[8]
     ];
+
     let updateHeader = function() {
         header.textContent = `Player ${currentPlayer.id}'s Turn`;
     }
+
+    let winHeader = function(player) {
+        if (player.id == 1 || player.id == 2) {
+            header.textContent = `Player ${player.id} wins!`
+        }
+
+        else {
+            header.textContent = 'Its a Tie!!';
+        }
+    }
+    
+    let winningMoves = [
+        //Vertical wins
+        [board[0], board[3], board[6]],
+        [board[1], board[4], board[7]],
+        [board[2], board[5], board[8]],
+        //Horizontal wins
+        [board[0], board[1], board[2]],
+        [board[3], board[4], board[5]],
+        [board[6], board[7], board[8]],
+        //Diag wins
+        [board[0], board[4], board[8]],
+        [board[6], board[4], board[2]]
+
+    ]
+    
     return {
         board,
         updateHeader,
+        winningMoves,
+        winHeader,
     }
 })();
 
 //Where the game is controlled
 const game = (() => {
 
+    //Checks for both players and lets the one with true turn make a mark, then toggles the turns to opposite on both
     let validateChoice = function(index) {
         if (player1.turn == true) {
             gameBoard.board[index].textContent = player1.mark;
@@ -32,17 +62,56 @@ const game = (() => {
             player2.turn = true;
             currentPlayer = player2;
             gameBoard.updateHeader();
+            game.checkWin();
         }
+
         else if(player2.turn == true) {
             gameBoard.board[index].textContent = player2.mark;
             player1.turn = true;
             player2.turn = false;
             currentPlayer = player1;
             gameBoard.updateHeader();
+            game.checkWin();
         }
     }
+
+    let checkWin = function() {
+        gameBoard.winningMoves.forEach(group =>{
+            let p1Win = group.every(element => {
+                if (element.textContent == 'X') {
+                    //Return player1 wins
+                    return true;
+                }
+            })
+            let p2Win = group.every(element => {
+                if (element.textContent == 'O') {
+                    //Return player2 wins
+                    return true;
+                }
+            })
+            let tie = gameBoard.board.every(element => {
+                if (element.textContent != '') {
+                    //Retunrs a tie because all cells are filled
+                    return true;
+                }
+            })
+            
+            if (p1Win == true) {
+                gameBoard.winHeader(player1);
+            }
+            else if (p2Win == true) {
+                gameBoard.winHeader(player2);
+            }
+            else if (tie == true) {
+                //Sends non player argument to winHeader to trigger tie 
+                gameBoard.winHeader('tie');
+            }
+        })
+    }
+
     return {
         validateChoice,
+        checkWin,
     }
 })();
 
@@ -62,7 +131,7 @@ function Player(id, turn, mark) {
 
 move.forEach(move => {
     move.addEventListener('click', function() {
-        if (move.textContent == '') {
+        if (move.textContent == '' && !header.textContent.includes('wins')) {
             let index = Array.from(move.parentElement.children).indexOf(move);
             game.validateChoice(index);
         }
