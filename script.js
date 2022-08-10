e2543f275a8db198211dba536e1e00c93685cf4b
Treyce Wheeler <t.zeryon@gmail.com>
2022-08-10 17:18:22 -0400

    Set up player switching and header change for current player

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //Selects the valid places to make a move
 let move = document.querySelectorAll('.move');
+let header = document.querySelector('.header');
+
+let player1 = Player(1,true,'X');
+let player2 = Player(2,false,'O');
+let currentPlayer = player1;
 
 //Module for the gameboard
 const gameBoard = (() => {
@@ -8,20 +13,36 @@ const gameBoard = (() => {
         move[3], move[4], move[5],
         move[6], move[7], move[8]
     ];
-    return {board}
-})();
-
-//Where game is displayed
-const displayGame = (() => {
-    const {board} = gameBoard;
-    
-    return{}
+    let updateHeader = function() {
+        header.textContent = `Player ${currentPlayer.id}'s Turn`;
+    }
+    return {
+        board,
+        updateHeader,
+    }
 })();
 
 //Where the game is controlled
 const game = (() => {
+
+    let validateChoice = function(index) {
+        if (player1.turn == true) {
+            gameBoard.board[index].textContent = player1.mark;
+            player1.turn = false;
+            player2.turn = true;
+            currentPlayer = player2;
+            gameBoard.updateHeader();
+        }
+        else if(player2.turn == true) {
+            gameBoard.board[index].textContent = player2.mark;
+            player1.turn = true;
+            player2.turn = false;
+            currentPlayer = player1;
+            gameBoard.updateHeader();
+        }
+    }
     return {
-        
+        validateChoice,
     }
 })();
 
@@ -35,16 +56,17 @@ function Player(id, turn, mark) {
             //Sets player turn to oppsite of current
             player.turn = !player.turn;
             console.log(player)
-        },
-        //MAYBE PUT EVENT LISTENER HERE I DUNNO
+        }
     }
 }
 
 move.forEach(move => {
     move.addEventListener('click', function() {
-        //Stores index of clicked element in variable
-        let index = Array.from(move.parentElement.children).indexOf(move);
-    });
+        if (move.textContent == '') {
+            let index = Array.from(move.parentElement.children).indexOf(move);
+            game.validateChoice(index);
+        }
+    })
 })
-let player1 = Player(1,true,'X');
-let player2 = Player(2,false,'O');
+
+gameBoard.updateHeader();
